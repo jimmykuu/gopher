72e1ebe110f99405715861c53baf0ee9effc4cff
jimmykuu <jimmy.kuu@gmail.com>
2013-09-04 18:44:18 +0800

    处理邮件发送错误，防止崩溃

--- src/sendmail/sendmail.go
+++ src/sendmail/sendmail.go
@@ -21,8 +21,12 @@ func main() {
 	for _, user := range users {
 		subject := "Golang中国域名改为golangtc.com"
 		message := user.Username + "，您好！\n\n由于golang.tc域名被Godaddy没收，现已不可继续使用，现在开始使用golangtc.com域名。希望继续参与到社区建设中来。\n\n Golang中国"
-		webhelpers.SendMail(subject, message, gopher.Config.FromEmail, []string{user.Email}, smtpConfig, false)
+		err := webhelpers.SendMail(subject, message, gopher.Config.FromEmail, []string{user.Email}, smtpConfig, false)
 
-		fmt.Println("send to:", user.Username, user.Email)
+		if err != nil {
+			fmt.Println("error: ", err.Error())
+		} else {
+			fmt.Println("send to:", user.Username, user.Email)
+		}
 	}
 }
