39596267a4fd66a280a9e242df1ddc2fb8aaa2ec
ggaaooppeenngg <471160161@qq.com>
2014-05-12 17:18:19 +0800

    缓冲一周的内容

--- src/gopher/rss.go
+++ src/gopher/rss.go
@@ -1,25 +1,37 @@
 package gopher
 
 import (
+	"container/list"
 	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 
 	"labix.org/v2/mgo"
 	//"labix.org/v2/mgo/bson"
 )
 
+//rss contents of today
+
+var contents []Topic
+
+//缓存
+var (
+	cache     list.List
+	beginTime time.Time
+)
+
 func rssHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("RSS")
 	session, err := mgo.Dial(Config.DB)
 	if err != nil {
 		panic(err)
 	}
 	defer session.Close()
 	session.SetMode(mgo.Monotonic, true)
+	now := time.Now()
+
 	DB := session.DB("gopher")
 	c := DB.C("contents")
-	var contents []Topic
 	c.Find(nil).All(&contents)
 	fmt.Println(contents)
 	t, err := template.ParseFiles("templates/rss.xml")
