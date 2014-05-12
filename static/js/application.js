8dce317af483eea7c2788d352de1e74d5b220a7d
Jimmy Kuu <jimmy.kuu@gmail.com>
2014-05-12 21:56:28 +0800

    页面增加回到顶部功能

--- static/js/application.js
+++ static/js/application.js
@@ -1,3 +1,21 @@
+function setToTop() {
+        $('body').append('<div id="toTop" title="回到顶部"><span class="glyphicon glyphicon-circle-arrow-up"></span></div>');
+        $(window).scroll(function() {
+        if ($(this).scrollTop()) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+        });
+
+        $("#toTop").click(function () {
+                //html works for FFX but not Chrome
+                //body works for Chrome but not FFX
+                //This strange selector seems to work universally
+                $("html, body").animate({scrollTop: 0}, 200);
+        });
+}
+
 $(document).ready(function(){
     // make code pretty
     window.prettyPrint && prettyPrint();
@@ -8,4 +26,6 @@ $(document).ready(function(){
         at: "@",
         data: 'http://www.golangtc.com/users.json'
     });
+
+    setToTop();
 });
