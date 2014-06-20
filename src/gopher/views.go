8f9b60f93c6b34e6e5d89ed01b59af1739e72fce
ggaaooppeenngg <471160171@qq.com>
2014-06-20 22:16:44 +0800

    暂存

--- src/gopher/views.go
+++ src/gopher/views.go
@@ -115,7 +115,7 @@ func (u *Utils) UserInfo(username string) template.HTML {
 
 /*mark ggaaooppeenngg*/
 func (u *Utils) RecentReplies(username string) template.HTML {
-	return template.HTML("")
+	//	return template.HTML("")
 
 	c := DB.C(USERS)
 	ccontens := DB.C(CONTENTS)
