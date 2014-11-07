17c569109c1fe7d43cb1bd03ae4934fb947e91d7
Jimmy Kuu <jimmy.kuu@gmial.com>
2014-11-07 17:12:37 +0800

    导航下载链接归类

--- static/js/application.js
+++ static/js/application.js
@@ -1,22 +1,30 @@
 function setToTop() {
-        $('body').append('<div id="toTop" title="回到顶部"><span class="glyphicon glyphicon-circle-arrow-up"></span></div>');
-        $(window).scroll(function() {
+    $('body').append('<div id="toTop" title="回到顶部"><span class="glyphicon glyphicon-circle-arrow-up"></span></div>');
+    $(window).scroll(function() {
         if ($(this).scrollTop()) {
             $('#toTop').fadeIn();
         } else {
             $('#toTop').fadeOut();
         }
-        });
+    });
 
-        $("#toTop").click(function () {
-                //html works for FFX but not Chrome
-                //body works for Chrome but not FFX
-                //This strange selector seems to work universally
-                $("html, body").animate({scrollTop: 0}, 200);
-        });
+    $("#toTop").click(function () {
+        //html works for FFX but not Chrome
+        //body works for Chrome but not FFX
+        //This strange selector seems to work universally
+        $("html, body").animate({scrollTop: 0}, 200);
+    });
 }
 
 $(document).ready(function(){
+	$(".dropdown-toggle").on("mouseover", function() {
+		if ($(this).parent().is(".open")) {
+			return;
+		}
+
+		$(this).dropdown("toggle")
+	});
+	
     // make code pretty
     window.prettyPrint && prettyPrint();
 
